3caded1226ceef3fee5b188f8131b437568c5fff
Tom Atkinson <me@tomatkinson.uk>
2023-12-08 21:44:00 +0000

    Initial commit

---
+++ src/instruction.rs
@@ -0,0 +1,58 @@
+#[derive(Debug, PartialEq)]
+pub enum Opcode {
+    LOAD,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    HLT,
+    JMP,
+    JMPF,
+    JMPB,
+    IGL
+}
+
+impl From<u8> for Opcode {
+    fn from(value: u8) -> Self {
+        match value {
+            0 => return Opcode::LOAD,
+            1 => return Opcode::ADD,
+            2 => return Opcode::SUB,
+            3 => return Opcode::MUL,
+            4 => return Opcode::DIV,
+            5 => return Opcode::HLT,
+            6 => return Opcode::JMP,
+            7 => return Opcode::JMPF,
+            8 => return Opcode::JMPB,
+            _ => return Opcode::IGL
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Instruction {
+    opcode: Opcode
+}
+
+impl Instruction {
+    pub fn new(opcode: Opcode) -> Instruction {
+        Instruction { opcode: opcode }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_hlt() {
+        let opcode = Opcode::HLT;
+        assert_eq!(opcode, Opcode::HLT);
+    }
+
+    #[test]
+    fn test_create_instruction() {
+        let instruction = Instruction::new(Opcode::HLT);
+        assert_eq!(instruction.opcode, Opcode::HLT);
+    }
+}
